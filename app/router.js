f6a460be1fa97b02595beede3b3b2aba04ea4d21
caishw <aishunairchandran@gmail.com>
2018-02-07 08:35:06 +1100

    First set of ember practice

--- app/router.js
+++ app/router.js
@@ -7,6 +7,16 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('orgs', {}); // /orgs
+
+  this.route('org', {path: 'org/:id'} ,function() { // org/ember
+    this.route('repos', {});
+    this.route('repo', {path:  ':rid'}, function() { // /org/jquery/jquery-ui
+      this.route('contributors', {});
+      this.route('issues',{});
+    });
+  });
+  this.route('notfound', {path:'*path'})
 });
 
 export default Router;
